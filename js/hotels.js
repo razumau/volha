1211588c965b4e3ac1a1e8fe247795eaccb37e9c
Jury Razumau <jurazumau@gmail.com>
2015-09-22 22:24:10 +0300

    add price fetching for hotels

--- js/hotels.js
+++ js/hotels.js
@@ -1,19 +1,37 @@
 (function($) {
 
-	//$.ajax();
-	//
-	
-	var $spans = $('[rel~=tooltip]'),
-		$span,
-		belarusianRubles,
-		russianRubles,
-		euros;
+	var byrToUsd, eurToByr, rubToByr;
 
-	for (var i = 0, len = $spans.length; i < len; i++) {
-		$span = $spans[i];
-		belarusianRubles = parseInt($span.text(), 10);
-		$spans[i].attr("title", euros + ' <i class="fa fa-eur"></i><br/>' +
-			russianRubles + ' <i class="fa fa-rub"></i>');
-	}
+	$.ajax({
+		url: 'https://openexchangerates.org/api/latest.json?app_id=ea8fd77aebdd496f9a815471963bd01a',
+		type: 'GET'
+	})
+	.done(function(data) {
+		var rates = data.rates;
+		eurToByr = rates.BYR / rates.EUR;
+		rubToByr = rates.BYR / rates.RUB;
+	})
+	.fail(function() {
+		eurToByr = 20000;
+		rubToByr = 270;
+	})
+	.always(function() {
+		var $spans = $('[rel~=tooltip]'),
+			$span,
+			price,
+			russianRubles,
+			euros;
+
+		for (var i = 0, len = $spans.length; i < len; i++) {
+			$span = $($spans[i]);
+			price = parseInt($span.text().replace(' ', ''), 10);
+
+			euros = (price / eurToByr).toFixed(1);
+			russianRubles = (price / rubToByr).toFixed(0);
+
+			$span.attr("title", euros + ' <i class="fa fa-eur"></i><br/>' +
+				russianRubles + ' <i class="fa fa-rub"></i>');
+		}
+	});
 
 })(jQuery);
